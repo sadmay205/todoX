d6f5480ce1690900b787f7adcc9155895d748a99
Nguyễn Tiến Giáp <tiengiap019065@gmail.com>
2025-12-31 18:45:28 +0700

    chuan bi deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,18 +3,30 @@ import tasksRouter from "./routes/tasksRouters.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 app.use('/api/tasks', tasksRouter);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
